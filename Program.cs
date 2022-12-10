84e22d4118e47cc297df1ed72d3bbacd8a82c4f4
LAY <achilova_94@list.ru>
2022-12-10 21:24:23 +0300

    Решено д.з

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+// // Задача 29: Напишите программу, которая задаёт массив из 8 элементов и 
+// // выводит их на экран.
+
+// // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// // 6, 1, 33 -> [6, 1, 33]
+
+int [] arr = new int [8];
+int n = arr.Length;
+Console.Write("Введите длину массива через запятую: ");
+Console.ReadLine();
+void FillArray(int[]arr)
+{
+for (int i = 0; i < arr.Length; i++);
+
+}
+
+void PrintArray(int[]arr)
+{
+{
+for (int i = 0; i < arr.Length; i++);
+
+}
+Console.Write("-> [" + String.Join(", ", arr) + "]");
+}
+
+FillArray(arr);
+PrintArray(arr);
